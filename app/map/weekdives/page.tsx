706d1edf1e3db4c7abfd13d4986ce29de1650900
APapailiou <148347194+APapailiou@users.noreply.github.com>
2024-04-11 17:59:41 +0300

    add Leeflet css

--- app/map/weekdives/page.tsx
+++ app/map/weekdives/page.tsx
@@ -11,6 +11,9 @@ import {
 import { PATH_MAP } from '@/routes';
 import { PageHeader, Surface } from '@/components';
 import { Metadata } from 'next';
+import { MapContainer, TileLayer, useMap, Marker, Popup, } from 'react-leaflet'
+import { LatLngExpression } from 'leaflet';
+import "leaflet/dist/leaflet.css";
 
 const items = [
   { title: 'Dashboard', href: PATH_MAP.public },
@@ -28,6 +31,8 @@ const PAPER_PROPS: PaperProps = {
   radius: 'md',
 };
 
+const position: LatLngExpression = [51.505, -0.09]
+
 function Weekdives() {
   return (
     <>
@@ -38,7 +43,17 @@ function Weekdives() {
           content="Weekly dives from dive centers newsletters."
         />   
       </>
-
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "90vh" }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
     </>
   );
 }
