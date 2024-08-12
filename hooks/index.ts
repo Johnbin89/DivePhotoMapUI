a8598d750b360e0f526a35b2e78d4756f8dee009
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-08-12 21:45:40 +0300

    add hook useMounted

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,5 +1,6 @@
 import useFetchData from '@/hooks/useFetchData';
 import useFetchSWR from '@/hooks/useFetchSWR';
 import useFetchSWRWithParams from '@/hooks/useFetchSWRWithParams';
+import useMounted from '@/hooks/useMounted';
 
-export { useFetchData, useFetchSWR, useFetchSWRWithParams };
+export { useFetchData, useFetchSWR, useFetchSWRWithParams, useMounted };
---
+++ hooks/useMounted.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+
+const useMounted = () => {
+    const [mounted, setMounted] = useState<boolean>()
+    // effects run only client-side
+    // so we can detect when the component is hydrated/mounted
+    // @see https://react.dev/reference/react/useEffect
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+    return mounted
+  }
+  
+  export default useMounted;
