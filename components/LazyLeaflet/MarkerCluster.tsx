91144af002a84ed50c78053b019fd7d307473e2e
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-08-24 04:23:18 +0300

    use classNames css in MarkerCluster

--- components/LazyLeaflet/MarkerCluster.tsx
+++ components/LazyLeaflet/MarkerCluster.tsx
@@ -1,5 +1,5 @@
 import MarkerClusterGroup from '@changey/react-leaflet-markercluster'
-import './MarkerCluster.module.css';
+import styles from './MarkerCluster.module.css';
 import type { FC, ReactNode } from 'react'
 import React from 'react'
 const MarkerCluster: FC<{
@@ -7,6 +7,7 @@ const MarkerCluster: FC<{
 }> = ({ children }) => {
   return (
     <MarkerClusterGroup
+      classNames={styles}
       showCoverageOnHover={false}
     >
       {children}
